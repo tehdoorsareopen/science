13829981c5b195beed83b6438bbc2595e832c9c2
eaklenov <eaklenov@gmail.com>
2018-02-22 15:52:59 +0300

    upd

--- Литература/books/IaaS/IaaS вступает в пору созревания_files/comments_data/Z_XoWik28EJ.js
+++
@@ -1,5 +0,0 @@
-/*1361901117,178142515*/
-
-if (self.CavalryLogger) { CavalryLogger.start_js(["ocqUg"]); }
-
-__d("legacy:developer-comments-js",["CommentAdminPanelController"],function(a,b,c,d){a.CommentAdminPanelController=b('CommentAdminPanelController');},3);
